2357821ccbd28c3aad52867ff8e4ff32153a398c
Karen <kfercosta11@gmail.com>
2023-10-13 21:15:55 -0300

    segundo commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesEjercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NotaCredito unaNotaCredito = new NotaCredito("NC-A", 00001, 1500);
+            Factura unaFactura = new Factura("F-A",00001,1500);
+
+            Impresora unaImpresora = new Impresora();
+            unaImpresora.Imprimir(unaFactura);
+            unaImpresora.Imprimir(unaNotaCredito);
+        }
+    }
+}
